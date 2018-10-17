ae3d68b76a4803c67ac0c2ae45c3de952ede891f
Pawel Plaszczyk <pawelp73@gmail.com>
2018-10-17 14:57:57 +0200

    adding state, removing dummy messages

--- src/App.js
+++ src/App.js
@@ -1,53 +1,56 @@
-import React from 'react';
-import CreateRoom from './components/CreateRoom';
-import MessageList from './components/MessageList';
-import RoomList from './components/RoomList';
-import Chatkit from '@pusher/chatkit';
-import SendMessage from './components/SendMessage';
-import {tokenUrl, instanceLocator } from './config'
+import React from "react";
+import CreateRoom from "./components/CreateRoom";
+import MessageList from "./components/MessageList";
+import RoomList from "./components/RoomList";
+import Chatkit from "@pusher/chatkit";
+import SendMessage from "./components/SendMessage";
+import { tokenUrl, instanceLocator } from "./config";
 
-import './App.css';
+import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: []
+    };
+  }
 
   componentDidMount() {
     const chatManager = new Chatkit.ChatManager({
-        instanceLocator,
-        userId: 'Pawcio',
-        tokenProvider: new Chatkit.TokenProvider({
-            url: tokenUrl
-        })
-    })
+      instanceLocator,
+      userId: "Pawcio",
+      tokenProvider: new Chatkit.TokenProvider({
+        url: tokenUrl
+      })
+    });
 
-    chatManager.connect()
-    .then(currentUser => {
-        currentUser.subscribeToRoom({
-            roomId: 18415190,
-            hooks: {
-              onNewMessage: message => {
-                  console.log('message.text: ', message.text);
-              }
+    chatManager.connect().then(currentUser => {
+      currentUser.subscribeToRoom({
+        roomId: 18415190,
+        messageLimit: 20,
+        hooks: {
+          onNewMessage: message => {
+            this.setState({
+              messages: [...this.state.messages, message]
+            });
           }
-                
-            })
-        })
-    }
-
-
-
-
-
+        }
+      });
+    });
+  }
 
   render() {
-      return (
-          <div className="app">
-              <RoomList />
-              <MessageList />
-              <SendMessage />
-              <CreateRoom />
-          </div>
-      );
+    console.log('this.state.messages:', this.state.messages);
+    return (
+      <div className="app">
+        <RoomList />
+        <MessageList messages = {this.state.messages} />
+        <SendMessage />
+        <CreateRoom />
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
